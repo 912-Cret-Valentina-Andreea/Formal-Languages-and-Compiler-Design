d3169aa408126f4f993e8d8d1825f207beba099c
912-Costin-Carmen <72121558+912-Costin-Carmen@users.noreply.github.com>
2022-12-09 16:31:58 +0200

    Fixes

--- Lab5/src/Domain/Grammar.java
+++ Lab5/src/Domain/Grammar.java
@@ -1,5 +1,9 @@
 package Domain;
-
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 public class Grammar {
     String start;
     List<String> terminals;
