0446e09ea568adca397e95d4c7c2df58f88ebff3
912-Costin-Carmen <72121558+912-Costin-Carmen@users.noreply.github.com>
2023-01-13 12:19:25 +0200

    ParseTable

--- Lab5/src/Domain/ParseTable.java
+++ Lab5/src/Domain/ParseTable.java
@@ -11,7 +11,46 @@ public class ParseTable {
     public ParseTable(Parser parser){
         this.parser = parser;
         this.grammar = parser.grammar;
+        generateParseTable();
     }
+    public void generateParseTable(){
+        for(String terminal : grammar.getTerminals()){
+            parsingTable.put(new Pair<>(terminal, terminal), new Pair<>(List.of("POP"), -1));
+        }
 
+        parsingTable.put(new Pair<>("$", "$"), new Pair<>(List.of("ACC."), -1));
+
+        for(String nonterminal: grammar.getNonterminals()){
+            for(Production production: grammar.getProductionsForNonterminalLHS(nonterminal)){
+                var concatenationFirstRHS = concatenateFirstOfProduction(production.rightPart);
+                var hasEpsilon = false;
+                for(String symbolFirst: concatenationFirstRHS){
+                    if(!symbolFirst.equals("EPS")){
+                        parsingTable.put(new Pair<>(nonterminal, symbolFirst), new Pair<>(production.rightPart, grammar.getProductionIndex(production)));
+                    }
+                    else{
+                        hasEpsilon = true;
+                    }
+                }
+                if(hasEpsilon){
+                    for(String symbolFollow: parser.followSet.get(nonterminal)){
+                        if(symbolFollow == "EPS"){
+                            parsingTable.put(new Pair<>(nonterminal, "$"), new Pair<>(production.rightPart, grammar.getProductionIndex(production)));
+                        }
+                        else{
+                            parsingTable.put(new Pair<>(nonterminal, symbolFollow), new Pair<>(production.rightPart, grammar.getProductionIndex(production)));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private Set<String> concatenateFirstOfProduction(List<String> prodRHS){
+        var concatenation = parser.firstSet.get(prodRHS.get(0));
+        for(int i = 1; i<prodRHS.size(); i++){
+            //concatenation = concatenationSizeOne(concatenation, parser.firstSet.get(prodRHS.get(i)));
+        }
+        return concatenation;
+    }
 
 }
